450a8a64065206465b176a2dac26f17a8036bc01
EugenHotaj <eugen_hotaj_91@hotmail.com>
2020-06-04 21:53:52 -0400

    Add examples/sec calculations to training code. Refactor.

--- colab_utils.py
+++ colab_utils.py
@@ -4,6 +4,7 @@ Warning: This module must be imported from Colab, otherwise it will crash.
 """
 
 import collections
+import time
 import os
 
 import PIL
@@ -119,39 +120,57 @@ def animate(frames, figsize=None, fps=24):
   return anim
 
 
-def _train_one_epoch(model, loss_fn, optimizer, train_loader, device):
-    model.train()
-    train_loss, n_examples = 0., 0
-    for x, y in train_loader:
+def _run_one_epoch(batch_fn, loader, device):
+    """Runs and instruments one epoch.
+
+    Args:
+        batch_fn: A fn(Xs, ys)->loss which takes in a batch of data and returns
+            a scalar loss. The batch_fn can do whatever it wants, such as 
+            train a model on the batch, eval on the batch, etc. 
+        loader: A DataLoader to run.
+        device: The torch 
+    """
+    start_time = time.time()
+    loss, n_examples = 0., 0
+    for x, y, in loader:
+        x, y = x.to(device), y.to(device)
         batch_size = x.shape[0]
         n_examples += batch_size
+        batch_loss = batch_fn(x, y)
+        loss += batch_loss.item() * batch_size
+    total_time = time.time() - start_time
+    examples_per_sec = round(n_examples / total_time)
+    loss /= n_examples
+    return loss, examples_per_sec
+    
 
+def _train_one_epoch(model, loss_fn, optimizer, train_loader, device):
+
+    def _train_batch_fn(x, y):
         optimizer.zero_grad()
-        x = x.to(device)
         preds = model(x)
         loss = loss_fn(x, y, preds)
         loss.backward()
         optimizer.step()
+        return loss
 
-        train_loss += loss.item() * batch_size
-    return train_loss / n_examples
+    model.train()
+    return _run_one_epoch(_train_batch_fn, train_loader, device)
 
 
 def _eval_one_epoch(model, loss_fn, eval_loader, device):
-    model.eval()
-    eval_loss, n_examples = 0., 0
-    with torch.no_grad():
-        for x, y in eval_loader:
-            batch_size = x.shape[0]
-            n_examples += batch_size
 
-            x = x.to(device)
+    def _eval_batch_fn(x, y):
+        with torch.no_grad():
             preds = model(x)
-            loss = loss_fn(x, y, preds)
-            eval_loss += loss.item() * batch_size
-    return eval_loss / n_examples
+            return loss_fn(x, y, preds)
+
+    model.eval()
+    return _run_one_epoch(_eval_batch_fn, eval_loader, device)
 
 
+# TODO(eugenhotaj): Should we just remove device completly and always call
+# get_device?
 def train_andor_evaluate(model,
                          loss_fn,
                          optimizer=None,
@@ -176,21 +195,23 @@ def train_andor_evaluate(model,
     Returns:
         (train_losses, eval_losses) per epoch.
     """
-    model = model.to(device)
-
-    train_losses = []
-    eval_losses = []
     assert train_loader is not None or eval_loader is not None, \
            'train_loader and eval_loader cannot both be None'
     if train_loader is not None:
         assert optimizer is not None, 'optimizer must be provided for training'
         assert n_epochs >= 1, 'n_epochs must be >= 0 for training'
 
+    model = model.to(device)
+
+    train_losses, eval_losses = [], []
+    if train_loader:
         for epoch in range(1, n_epochs + 1):
-            train_loss = _train_one_epoch(model, loss_fn, optimizer, 
-                                          train_loader, device)
-            eval_loss = _eval_one_epoch(model, loss_fn, eval_loader, device)
-            print(f'[{epoch}]: train_loss={train_loss} eval_loss={eval_loss}')
+            train_loss, examples_per_sec = _train_one_epoch(
+                    model, loss_fn, optimizer, train_loader, device)
+            eval_loss, _ = _eval_one_epoch(
+                    model, loss_fn, eval_loader, device)
+            print(f'[{epoch} | {examples_per_sec}]: train_loss={train_loss} '
+                  f'eval_loss={eval_loss}')
             train_losses.append(train_loss)
             eval_losses.append(eval_loss)
     else:

4f8977d905d69bbe64ca20728c95d6ff2179b3a7
EugenHotaj <eugen_hotaj_91@hotmail.com>
2020-07-23 20:07:42 -0400

    Make models accessible from top level module.

---
+++ pytorch_generative/models/__init__.py
@@ -0,0 +1,7 @@
+"""Models available in PyTorch Generative."""
+
+from pytorch_generative.models import GatedPixelCNN
+from pytorch_generative.models import MADE
+from pytorch_generative.models import NADE
+
+__all__ = ['GatedPixelCNN', 'MADE', 'NADE']

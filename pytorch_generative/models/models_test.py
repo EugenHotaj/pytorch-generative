7e58db92ff0370e5d5595a742321086e56866a04
EugenHotaj <eugen_hotaj_91@hotmail.com>
2020-10-04 15:34:05 -0400

    Update tests to use multiple channels.
    This is more general than testing 1 channel.

--- pytorch_generative/models/models_test.py
+++ pytorch_generative/models/models_test.py
@@ -7,9 +7,6 @@ from torch import distributions
 
 from pytorch_generative import models
 
-def _multi_channel_test_sample_fn(x):
-  x = x.reshape(2, 3, 10)
-  return distributions.Categorical(logits=x).sample().type(torch.float32)
 
 class ModelSmokeTestCase(unittest.TestCase):
 
@@ -31,8 +28,8 @@ class ModelSmokeTestCase(unittest.TestCase):
   # TODO(eugenhotaj): Use parameterized tests instead of creating a new method
   # for each model.
   def test_TinyCNN(self):
-    model = models.TinyCNN(in_channels=1)
-    self._smoke_test(model)
+    model = models.TinyCNN(in_channels=3, out_channels=3)
+    self._smoke_test(model, in_channels=3)
 
   def test_NADE(self):
     model = models.NADE(input_dim=25, hidden_dim=5)
@@ -43,39 +40,37 @@ class ModelSmokeTestCase(unittest.TestCase):
     self._smoke_test(model)
 
   def test_PixelCNN(self):
-    model = models.PixelCNN(in_channels=1,
-                            out_channels=1,
+    model = models.PixelCNN(in_channels=3,
+                            out_channels=3,
                             n_residual=1,
                             residual_channels=1,
                             head_channels=1)
-    self._smoke_test(model)
+    self._smoke_test(model, in_channels=3)
 
   def test_GatedPixelCNN(self):
-    model = models.GatedPixelCNN(in_channels=1,
-                                 out_channels=1,
+    model = models.GatedPixelCNN(in_channels=3,
+                                 out_channels=3,
                                  n_gated=1,
                                  gated_channels=1,
                                  head_channels=1)
-    self._smoke_test(model)
+    self._smoke_test(model, in_channels=3)
 
   def test_PixelSNAIL(self):
     model = models.PixelSNAIL(in_channels=3,
-                              out_channels=3*10,
+                              out_channels=3,
                               n_channels=1,
                               n_pixel_snail_blocks=1,
                               n_residual_blocks=1,
                               attention_key_channels=1,
                               attention_value_channels=1,
-                              head_channels=1,
-                              sample_fn=_multi_channel_test_sample_fn)
+                              head_channels=1)
     self._smoke_test(model, in_channels=3) 
 
   def test_ImageGPT(self):
-    # Test ImageGPT using MaskedAttention.
-    model = models.ImageGPT(in_channels=1,
+    model = models.ImageGPT(in_channels=3,
                             in_size=5,
-                            out_channels=1,
+                            out_channels=3,
                             n_transformer_blocks=1,
                             n_attention_heads=2,
                             n_embedding_channels=4)
-    self._smoke_test(model)
+    self._smoke_test(model, in_channels=3)

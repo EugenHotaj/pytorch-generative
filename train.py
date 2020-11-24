c562c246ad087c986fd445ae1fd07cd8e36babc3
EugenHotaj <eugen_hotaj_91@hotmail.com>
2020-11-23 22:44:13 -0600

    Update train script to use the new reproduce functions

--- train.py
+++ train.py
@@ -13,61 +13,40 @@ from torchvision import transforms
 import pytorch_generative as pg	
 
 
-MODEL_MAP = {	
-    'gated_pixel_cnn': pg.models.GatedPixelCNN,	
-    'image_gpt': pg.models.ImageGPT,
-    'made': pg.models.MADE,	
-    'nade': pg.models.NADE,	
-    'pixel_cnn': pg.models.PixelCNN,	
-    'pixel_snail': pg.models.PixelSNAIL,	
-    'vae': pg.models.VAE,
-    'vq_vae': pg.models.VQVAE,
-    'vq_vae_2': pg.models.VQVAE2,
-    'tiny_cnn': pg.models.TinyCNN,	
+MODEL_DICT = {	
+    'gated_pixel_cnn': pg.models.gated_pixel_cnn,
+    'image_gpt': pg.models.image_gpt,
+    'made': pg.models.made,
+    'nade': pg.models.nade,
+    'pixel_cnn': pg.models.pixel_cnn,
+    'pixel_snail': pg.models.pixel_snail,
+    'vae': pg.models.vae,
+    'vq_vae': pg.models.vq_vae,
+    'vq_vae_2': pg.models.vq_vae_2,
 }	
 
 
 def main(args):	
-  transform=transforms.Compose([	
-      transforms.ToTensor(),	
-      transforms.Lambda(lambda x: distributions.Bernoulli(probs=x).sample())	
-  ])	
-  train_loader = torch.utils.data.DataLoader(	
-          datasets.MNIST('./data', train=True, download=True, transform=transform),	
-          batch_size=args.batch_size, shuffle=True)	
-  test_loader = torch.utils.data.DataLoader(	
-          datasets.MNIST('./data', train=False, download=True, transform=transform),	
-          batch_size=args.batch_size)	
-
-  model = MODEL_MAP[args.model](in_channels=1, out_channels=1)	
-  optimizer = optim.Adam(model.parameters())	
-  scheduler = lr_scheduler.MultiplicativeLR(optimizer, lambda _: 0.9984)	
-
-  criterion = nn.BCEWithLogitsLoss(reduction='none')	
-  def loss_fn(x, _, preds):	
-    batch_size = x.shape[0]	
-    x, preds = x.view((batch_size, -1)), preds.view((batch_size, -1))	
-    return criterion(preds, x).sum(dim=1).mean()	
-
-  trainer = pg.trainer.Trainer(	
-      model, loss_fn, optimizer, train_loader, test_loader, 	
-      lr_scheduler=scheduler, log_dir=args.log_dir, save_checkpoint_epochs=1)	
-  trainer.interleaved_train_and_eval(n_epochs=args.n_epochs)	
+  device = 'cuda' if args.use_cuda else'cpu'
+  MODEL_DICT[args.model].reproduce(args.n_epochs, args.batch_size, args.log_dir, 
+                                   device) 
 
 
 if __name__ == '__main__':	
   parser = argparse.ArgumentParser()	
   parser.add_argument(	
-      '--log_dir', type=str, help='the directory where to log data',	
-      default='/tmp/runs')	
-  parser.add_argument(	
       '--model', type=str, help='the available models to train', 	
-      default='tiny_cnn', choices=list(MODEL_MAP.keys()))	
+      default='nade', choices=list(MODEL_DICT.keys()))	
   parser.add_argument(	
-      '--batch_size', type=int, help='the training and evaluation batch_size', 	
+      '--n-epochs', type=int, help='number of training epochs', default=1)	
+  parser.add_argument(	
+      '--batch-size', type=int, help='the training and evaluation batch_size', 	
       default=128)	
   parser.add_argument(	
-      '--n_epochs', type=int, help='number of training epochs', default=1)	
+      '--log-dir', type=str, help='the directory where to log data',	
+      default='/tmp/run')	
+  parser.add_argument(
+      '--use-cuda', help='whether to use CUDA', action='store_true')
   args = parser.parse_args()	
 
   main(args)

d1957104442d85e18953aa4f7ea8679889a42411
EugenHotaj <eugen_hotaj_91@hotmail.com>
2020-11-25 16:03:09 -0600

    Simply the VAE code.
    We now use convolutions to compute the latents instead of linear layers.
    This allows us to simplify the model architecture a bit. It also gets
    rid of the need to take an in_size argument.

--- pytorch_generative/models/vae.py
+++ pytorch_generative/models/vae.py
@@ -7,81 +7,66 @@ from pytorch_generative.models import vaes
 
 
 class VAE(nn.Module):
-    """The Variational Autoencoder Model."""
+    """The Variational Autoencoder model."""
 
     def __init__(
         self,
         in_channels=1,
         out_channels=1,
-        in_size=28,
-        latent_dim=10,
-        hidden_channels=32,
-        n_residual_blocks=2,
-        residual_channels=16,
+        latent_channels=2,
+        hidden_channels=128,
+        residual_channels=32,
     ):
         """Initializes a new VAE instance.
 
         Args:
             in_channels: Number of input channels.
             out_channels: Number of output channels.
-            in_size: Size of the input images. Used to create bottleneck layers.
-            latent_dim: The dimensionality of each latent variable.
+            latent_channels: Number of channels for each latent variable.
             hidden_channels: Number of channels in (non residual block) hidden layers.
-            n_residual_blocks: Number of residual blocks in each residual stack.
             residual_channels: Number of hidden channels in residual blocks.
         """
         super().__init__()
 
-        self._latent_dim = latent_dim
-        self._encoder_out_dims = (hidden_channels, in_size // 4, in_size // 4)
+        self._latent_channels = latent_channels
+        self._stride = 4
 
         self._encoder = vaes.Encoder(
             in_channels=in_channels,
-            out_channels=hidden_channels,
+            out_channels=2 * self._latent_channels,
             hidden_channels=hidden_channels,
             residual_channels=residual_channels,
-            n_residual_blocks=n_residual_blocks,
-            stride=4,
+            n_residual_blocks=2,
+            stride=self._stride,
         )
-        flat_dim = hidden_channels * in_size // 4 * in_size // 4
-        self._mean = nn.Linear(flat_dim, self._latent_dim)
-        self._log_var = nn.Linear(flat_dim, self._latent_dim)
-        self._bottleneck = nn.Linear(self._latent_dim, flat_dim)
-        self._decoder_ = vaes.Decoder(
-            in_channels=hidden_channels,
+        self._decoder = vaes.Decoder(
+            in_channels=self._latent_channels,
             out_channels=out_channels,
             hidden_channels=hidden_channels,
             residual_channels=residual_channels,
-            n_residual_blocks=n_residual_blocks,
-            stride=4,
+            n_residual_blocks=2,
+            stride=self._stride,
         )
 
-    def _decoder(self, x):
-        n, c, h, w = x.shape[0], *self._encoder_out_dims
-        x = self._bottleneck(x).view(n, c, h, w)
-        return self._decoder_(x)
-
     def forward(self, x):
-        encoded = self._encoder(x).view(x.shape[0], -1)
+        # TODO(eugenhotaj): Maybe take in_size as input arguments in sample()?
+        self._in_size = x.shape[2]  # Save for later sampling.
         # NOTE: We use log_var (instead of var or std) for stability and easier
         # optimization during training.
-        mean, log_var = self._mean(encoded), self._log_var(encoded)
+        mean, log_var = torch.split(self._encoder(x), self._latent_channels, dim=1)
         var = torch.exp(log_var)
         latents = mean + torch.sqrt(var) * torch.randn_like(var)
         # NOTE: This KL divergence is only applicable under the assumption that the
         # prior ~ N(0, 1) and the latents are Gaussian.
-        # NOTE: Technically, the KL divergence computation requires a sum over the
-        # latent dimension. However, this makes the magnitude of the KL divergence
-        # dependent on the dimensionality of the latents. Instead, we divide the
-        # KL divergence by the dimension of the latents, which is equivalent to
-        # averaging over the latent dimensions.
-        kl_div = -0.5 * (1 + log_var - mean ** 2 - var).mean()
+        kl_div = -0.5 * (1 + log_var - mean ** 2 - var).mean(dim=(1, 2, 3))
         return self._decoder(latents), kl_div
 
-    def sample(self, n_images):
-        """Generates a batch of n_images."""
+    def sample(self, n_samples):
+        """Generates a batch of n_samples."""
         device = next(self.parameters()).device
-        latents = torch.randn((n_images, self._latent_dim), device=device)
+        latent_size = self._in_size // self._stride
+        shape = (n_samples, self._latent_channels, latent_size, latent_size)
+        latents = torch.randn(shape, device=device)
         return self._decoder(latents)
 
 
@@ -128,27 +113,26 @@ def reproduce(
     model = models.VAE(
         in_channels=1,
         out_channels=1,
-        in_size=28,
-        latent_dim=10,
-        hidden_channels=32,
-        n_residual_blocks=2,
-        residual_channels=16,
+        latent_channels=2,
+        hidden_channels=128,
+        residual_channels=32,
     )
     optimizer = optim.Adam(model.parameters(), lr=1e-3)
     scheduler = lr_scheduler.MultiplicativeLR(optimizer, lr_lambda=lambda _: 0.999977)
 
     def loss_fn(x, _, preds):
         preds, vae_loss = preds
-        recon_loss = F.binary_cross_entropy_with_logits(preds, x)
-        loss = recon_loss * 100 + vae_loss
+        recon_loss = F.binary_cross_entropy_with_logits(preds, x, reduction="none")
+        recon_loss = recon_loss.mean(dim=(1, 2, 3))
+        loss = recon_loss + vae_loss
         return {
-            "recon_loss": recon_loss,
-            "vae_loss": vae_loss,
-            "loss": loss,
+            "recon_loss": recon_loss.mean(),
+            "vae_loss": vae_loss.mean(),
+            "loss": loss.mean(),
         }
 
     def sample_fn(model):
-        return torch.sigmoid(model.sample(n_images=64))
+        return torch.sigmoid(model.sample(n_samples=16))
 
     model_trainer = trainer.Trainer(
         model=model,
